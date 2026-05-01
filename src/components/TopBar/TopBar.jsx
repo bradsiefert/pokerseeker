f3720acca6bfe2ff36884359b035e36ed6a9f041
Brad Siefert <bradsiefert@icloud.com>
2026-04-30 23:22:49 -0500

    Adding topbar

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,9 @@
+import styles from './TopBar.module.css'
+
+export default function TopBar() {
+  return (
+    <div className={styles.topBar}>
+      <span className={styles.title}>Pokerseeker</span>
+    </div>
+  )
+}
